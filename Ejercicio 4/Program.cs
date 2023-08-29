7d9bc57114186936c6729589e7a7d9e1f4158fe8
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-08-29 01:50:09 -0500

    Add files via upload

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el tiempo de revisión en minutos para el salón A:");
+            int tiempoSalonA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tiempo de revisión en minutos para el salón B:");
+            int tiempoSalonB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tiempo de revisión en minutos para el salón C:");
+            int tiempoSalonC = Convert.ToInt32(Console.ReadLine());
+
+            int tiempoTotalMinutos = tiempoSalonA + tiempoSalonB + tiempoSalonC;
+
+            int horas = tiempoTotalMinutos / 60;
+            int minutos = tiempoTotalMinutos % 60;
+
+            Console.WriteLine($"El instructor tardará {horas} horas y {minutos} minutos en revisar los exámenes de todos los salones.");
+        }
+    }
+}
